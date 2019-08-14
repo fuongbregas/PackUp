d6dade5eb92afd672d06c55933398855cbf05630
Fuongbregas <fuongbregas@icloud.com>
2019-08-13 18:51:15 -0700

    Fix undefined folder_path

--- startScreen.py
+++ startScreen.py
@@ -1,29 +1,30 @@
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 from tkinter import *
+import mainScreen
 
 def browse_button():
     # Allow user to select a directory and store it in global var
     # called folder_path
-    global folder_path
     filename = filedialog.askdirectory()
-    folder_path.set(filename)
+    #global folder_path
+    #folder_path.set(filename)
     fileLocation = open("folderLocation.txt", "w")
     fileLocation.write(filename)
     print(filename)
 
 def browse_file():
     #Allow user to search the setting file
-    global folder_path
+    #global folder_path
     settingsFile = askopenfile(title='Open Settings File',filetypes=[('Settings File','*.set')])
     lines = settingsFile.readline()
     folderLocation = open("folderLocation.txt", "w")
     folderLocation.write(lines)
     print(lines)
+    mainScreen.runMainScreen()
 
-def runMainScreen():
+def runStartScreen():
     mainFrame = Tk()
-    folder_path = StringVar()
     topFrame = Frame(mainFrame, width=100, height=100)
     topFrame.pack()
 
